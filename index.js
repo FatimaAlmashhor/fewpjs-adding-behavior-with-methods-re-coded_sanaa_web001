8e65cadefa7e8e99d462f2c9f7c9d5944b8d38b8
Fatima Amin <fatima.amin4official@gmail.com>
2020-09-17 12:10:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
 // Your code here
+class animal{
+   constractor(name , gender) {
+    this.name = name ;
+    this.gender = gender ;
+  }
+  speak(){
+    
+  }
+}
+class Cat extends animal{
+  constractor(name , gender) {
+   this.name = name ;
+    this.gender = gender ;
+  }
+  speak(){
+    returns `${this.name} says meow!`;
+  }
+}
+
+ cat = new Cat("Sasha", "female");
+ cat.speak()
+
+class Dog  extends animal{
+  constractor(name , gender) {
+    this.name = name ;
+    this.gender = gender ;
+  }
+  speak(){
+    returns `${this.name} says woof!`;
+  }
+}
+
+let dog = new Dog("Rufio", "male");
+ dog.speak() ;
+ 
+ 
+ class Bird {
+  constractor(name , gender) {
+    this.name = name ;
+    this.gender = gender ;
+  }
+  speak(){
+    returns `It's me! ${this.name}, the parrot!`;
+  }
+}
+
+
+ let bird = new Bird("Pablo", "male")
+  bird.speak();
+    let  bird2 = new Bird("Mable", "female")
+    
+    
+
+
